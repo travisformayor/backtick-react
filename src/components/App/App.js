450082e7dee2e47c61c73d766bec76639327a39a
Travis Uhrig <travis.uhrig@gonimbly.com>
2020-08-25 17:01:18 -0700

    theme and header

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Header from '../Header/Header';
+// import ResultsHolder from '../ResultsHolder/ResultsHolder';
+// import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      {/* <ResultsHolder /> */}
+    </>
+  );
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import SearchBar from '../SearchBar/SearchBar';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    textAlign: 'center',
+    fontSize: '4rem',
+    margin: '10px',
+  },
+  search: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+  },
+}));
+
+export default function App() {
+  const style = useStyles();
+
+  return (
+    <>
+      <p className={style.title}>Backtick`</p>
+      <div className={style.search}>
+        <SearchBar />
+      </div>
+    </>
+  );
+}
