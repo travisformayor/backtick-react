411eb6622d7ca815fcbad700de556506334d71f1
Travis Uhrig <travis.uhrig@gonimbly.com>
2020-08-27 11:28:57 -0700

    reset results between searches

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,16 @@
 // to dos:
-// scroll switches to mini-header for easy followup searches
-// header displays total found
-// scroll lazy loads more results
-// hover over cards effect
+// - header displays total found
+// - scroll lazy loads more results
+// - hover over cards effect
+// - gray out/indicate cards for already visited links
+// animations:
+// - header flex switch and shrink
+// - cards fade/pop in staggered first to last
+// - on new search, cards fade/pop first to last while waiting for backend
+// - header black at first, title white with black shadow
+// - once mini image loads, fade in blurry version
+// - once full image loads, fade in over blurry version
+// - can fade in be splotchy and non-uniform, like a watercolor effect
 
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
@@ -25,26 +33,30 @@ export default function App() {
   }, [urlParams]);
 
   useEffect(() => {
+    const blank = {
+      'results': null,
+      'query': null,
+      'offset': 0,
+      'total': 0,
+    };
+
     if (searchTerm) {
       history.push(`/search=${searchTerm}`);
 
       fetchResults(searchTerm);
       async function fetchResults(searchTerm) {
         // Fetch results from the backend
+        // Set to blank first to reset old cards
+        setSearchData(blank);
         const endpoint = 'https://backtick.tilde.wtf';
         const res = await axios(`${endpoint}/search?q=${searchTerm}`);
         const data = await res.data;
+        console.log(data);
         setSearchData(data);
       }
     } else {
       history.push('/');
       // No search term. Set Blank
-      const blank = {
-        'results': null,
-        'query': null,
-        'offset': 0,
-        'total': 0,
-      };
       setSearchData(blank);
     }
   }, [searchTerm, history]);
