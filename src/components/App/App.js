d66feabad934a012451823afcb2a996773c3bf1b
Travis <travisformayor@users.noreply.github.com>
2020-08-29 13:04:02 -0700

    load on scroll

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@
 // - scroll lazy loads more results
 // - hover over cards effect
 // - gray out/indicate cards for already visited links
+// - add link to cyrus blog post, backend, site readme/post
 // animations:
 // - header flex switch and shrink
 // - cards fade/pop in staggered first to last
@@ -11,6 +12,7 @@
 // - once mini image loads, fade in blurry version
 // - once full image loads, fade in over blurry version
 // - can fade in be splotchy and non-uniform, like a watercolor effect
+// - bouncing loading dots
 
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
@@ -21,51 +23,105 @@ import axios from 'axios';
 
 export default function App() {
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchData, setSearchData] = useState([]);
+  const [resultData, setResultData] = useState({ results: [] });
+  const [newOffset, setOffset] = useState(0);
+  const [loading, setLoading] = useState(false);
   const history = useHistory();
   const urlParams = useParams();
 
+  // === API Functions
+  async function endpointCallout(term, offset = 0) {
+    // Fetch Results
+    setLoading(true);
+    console.log(`Perform Search: ${term}`);
+    const endpoint = 'https://backtick.tilde.wtf';
+    const params = `search?q=${term}&offset=${offset}`;
+    console.log(`${endpoint}/${params}`);
+    const res = await axios(`${endpoint}/${params}`);
+    console.log('api response:');
+    console.log(res.data);
+    setLoading(false);
+    return res.data;
+  }
+
+  async function fetchMore() {
+    console.log('Current Loading: ' + loading);
+    console.log(searchTerm);
+    if (!loading) {
+      const data = await endpointCallout(searchTerm, newOffset);
+      setResultData({
+        results:
+          data.results && data.results.length > 0
+            ? [...resultData.results, ...data.results]
+            : resultData.results,
+        query: data.query ? data.query : null,
+        offset: data.offset ? parseInt(data.offset) : 0,
+        total: data.total ? parseInt(data.total) : 0,
+      });
+    } else {
+      console.log('already loading more...');
+    }
+  }
+
   useEffect(() => {
-    // Existing URL Params term. Save to term state
+    // == Handle url containing a search term
     if (urlParams && urlParams.search) {
       setSearchTerm(urlParams.search);
     }
   }, [urlParams]);
 
   useEffect(() => {
-    const blank = {
-      'results': null,
-      'query': null,
-      'offset': 0,
-      'total': 0,
-    };
-
+    // == Handle changed/new search term from url or prop callback
     if (searchTerm) {
+      console.log(`New search term detected: ${searchTerm}`);
       history.push(`/search=${searchTerm}`);
 
-      fetchResults(searchTerm);
-      async function fetchResults(searchTerm) {
-        // Fetch results from the backend
-        // Set to blank first to reset old cards
-        setSearchData(blank);
-        const endpoint = 'https://backtick.tilde.wtf';
-        const res = await axios(`${endpoint}/search?q=${searchTerm}`);
-        const data = await res.data;
-        console.log(data);
-        setSearchData(data);
+      setResultData({ results: [] }); // Clear previous search results
+
+      fetchResults();
+      async function fetchResults() {
+        const data = await endpointCallout(searchTerm);
+        setResultData({
+          results: data.results && data.results.length > 0 ? data.results : [],
+          query: data.query ? data.query : null,
+          offset: data.offset ? parseInt(data.offset) : 0,
+          total: data.total ? parseInt(data.total) : 0,
+        });
       }
     } else {
+      // searchTerm was changed to null
+      console.log(`Blank search`);
       history.push('/');
-      // No search term. Set Blank
-      setSearchData(blank);
+
+      setResultData({ results: [] }); // Clear previous search results
     }
   }, [searchTerm, history]);
+
+  useEffect(() => {
+    // == Handle more results for the same search term
+    const updateCriteria =
+      newOffset &&
+      newOffset > 0 && // Not the first offset
+      newOffset === resultData.results.length && // maxed out the current offset
+      resultData.results.length % 30 === 0 && // current results is a multiple of 30
+      resultData.results.length < resultData.total; // more results available
+
+    if (updateCriteria) {
+      console.log('New valid offset. Fetching more...');
+      fetchMore();
+    }
+    // https://reactjs.org/docs/hooks-faq.html#what-can-i-do-if-my-effect-dependencies-change-too-often
+  }, [newOffset]);
 
   return (
     <>
       <CssBaseline />
       <Header setSearchTerm={setSearchTerm} existingTerm={searchTerm} />
-      <ResultsContainer searchData={searchData} />
+      <ResultsContainer
+        resultData={resultData}
+        setOffset={setOffset}
+        loading={loading}
+      />
     </>
   );
 }
--- src/components/ResultsContainer/ResultsContainer.js
+++ src/components/ResultsContainer/ResultsContainer.js
@@ -5,20 +5,28 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   results: {
-    paddingTop: '160px',
-    margin: '10px 10px',
+    paddingTop: '180px',
+    marginRight: '10px',
+    marginLeft: '10px',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(300px, max-content))',
+    gridGap: '15px',
+    justifyContent: 'center',
+  },
+  card: {
+    opacity: 0,
+  },
+  loader: {
     display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
     justifyContent: 'center',
   },
 }));
 
-export default function ResultsContainer({ searchData }) {
+export default function ResultsContainer({ resultData, setOffset, loading }) {
+  // to do: use loading for triggering bouncing ball animation
   // const { results, query, offset, total } = searchData;
-  const { results, query } = searchData;
+  const { results, query, offset, total } = resultData;
   const style = useStyles();
-
   let cardRefs = [];
 
   function returnRef(ref) {
@@ -28,32 +36,65 @@ export default function ResultsContainer({ searchData }) {
   }
 
   useEffect(() => {
+    // Animate populating cards
+
+    // to do: if the card's opacity is already 1, skip/remove it from refs array
     if (cardRefs.length > 0) {
+      // card opacity set to 0 in ResultCard.js
       gsap.fromTo(
         cardRefs,
-        { y: 100 },
-        {
-          duration: 0.2,
-          opacity: 1, // opacity 0 set in ResultCard
-          y: 0,
-          stagger: 0.1,
-        }
+        { y: 50 },
+        { duration: 0.2, opacity: 1, y: 0, stagger: 0.05 }
       );
     }
-  });
+  }, [cardRefs]);
+
+  useEffect(() => {
+    // Load more cards on scroll
+    // to do: trigger before reaching the bottom
+    window.addEventListener('scroll', bottomScroll);
+    function bottomScroll() {
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        if (results.length < total) {
+          if (offset + 30 !== results.length) {
+            console.log('Unexpected number of results');
+            console.log(offset);
+            console.log(results);
+          } else {
+            console.log('Set new offset');
+            setOffset(offset + 30);
+          }
+        } else {
+          console.log('No more results');
+        }
+      }
+    }
+
+    // Remove event listener on component unmount
+    return () => window.removeEventListener('scroll', bottomScroll);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [resultData]);
 
   return (
-    <div className={style.results}>
-      {results === null && query ? <p>No Results for {query}</p> : ''}
-      {results
-        ? results.map((result, index) => (
-            <ResultCard
-              result={result}
-              returnRef={returnRef}
-              key={'result-card-' + index}
-            />
-          ))
-        : ''}
-    </div>
+    <>
+      <div className={style.results}>
+        {results === [] && query ? <p>No Results for {query}</p> : ''}
+        {results
+          ? results.map((result, index) => (
+              <ResultCard
+                result={result}
+                returnRef={returnRef}
+                key={'result-card-' + index}
+              />
+            ))
+          : ''}
+      </div>
+      <div className={style.loader}>
+        {loading ? <span>loading...</span> : <span></span>}
+      </div>
+    </>
   );
 }
