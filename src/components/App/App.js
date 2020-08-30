84c81d3c317395e71c12ddaa56b6c529bb74ffbb
Travis <travisformayor@users.noreply.github.com>
2020-08-29 18:40:01 -0700

    Move all search code into SearchClient.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,18 +15,14 @@
 // - bouncing loading dots
 // - fade in 'no more results' on attempts to scroll past end
 
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { gsap } from 'gsap';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Header from '../Header/Header';
-import ResultsContainer from '../ResultsContainer/ResultsContainer';
-import axios from 'axios';
+import SearchClient from '../SearchClient/SearchClient';
 
 export default function App() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [resultData, setResultData] = useState({ results: [] });
-  const [loading, setLoading] = useState(false);
+  // App.js handles top level code, like animations and url control
   const history = useHistory();
   const urlParams = useParams();
 
@@ -54,6 +50,7 @@ export default function App() {
 
   useEffect(() => {
     // Animate populating cards
+    console.log(refs.cards.length);
     if (refs.cards.length > 0) {
       console.log('=> run card animation');
       console.log(`Cards to animate: ${refs.cards.length}`);
@@ -101,100 +98,15 @@ export default function App() {
     }
     // Remove event listener on component unmount
     return () => window.removeEventListener('scroll', resizeOnScroll);
-  });
-
-  // === API Functions
-  async function endpointCallout(term, offset = 0) {
-    // Fetch Results
-    setLoading(true);
-    console.log(`Callout for term: ${term}`);
-    const endpoint = 'https://backtick.tilde.wtf';
-    const params = `search?q=${term}&offset=${offset}`;
-    console.log(`${endpoint}/${params}`);
-    const res = await axios(`${endpoint}/${params}`);
-    console.log('api response:');
-    console.log(res.data);
-    setLoading(false);
-    return res.data;
-  }
-
-  async function fetchMore(newOffset) {
-    const updateCriteria =
-      newOffset &&
-      newOffset > 0 && // Not the first offset
-      newOffset === resultData.results.length && // maxed out the current offset
-      resultData.results.length % 30 === 0 && // current results is a multiple of 30
-      !loading; // Not already performing a callout
-
-    if (updateCriteria) {
-      console.log('Valid offset request. Fetching more...');
-
-      const data = await endpointCallout(searchTerm, newOffset);
-      setResultData({
-        results:
-          data.results && data.results.length > 0
-            ? [...resultData.results, ...data.results]
-            : resultData.results,
-        query: data.query ? data.query : null,
-        offset: data.offset ? parseInt(data.offset) : 0,
-        total: data.total ? parseInt(data.total) : 0,
-      });
-    } else {
-      console.log('Failed requirements for fetchMore');
-    }
-  }
-
-  useEffect(() => {
-    console.log(`Loading State: ${loading}`);
-  }, [loading]);
-
-  useEffect(() => {
-    // == Handle url containing a search term
-    if (urlParams && urlParams.search) {
-      setSearchTerm(urlParams.search);
-    }
-  }, [urlParams]);
-
-  useEffect(() => {
-    // == Handle changed/new search term from url or prop callback
-    if (searchTerm) {
-      console.log(`New search term detected: ${searchTerm}`);
-      history.push(`/search=${searchTerm}`);
-
-      setResultData({ results: [] }); // Clear previous search results
-
-      fetchResults();
-      async function fetchResults() {
-        const data = await endpointCallout(searchTerm);
-        setResultData({
-          results: data.results && data.results.length > 0 ? data.results : [],
-          query: data.query ? data.query : null,
-          offset: data.offset ? parseInt(data.offset) : 0,
-          total: data.total ? parseInt(data.total) : 0,
-        });
-      }
-    } else {
-      // searchTerm was changed to null
-      console.log(`Blank search`);
-      history.push('/');
-
-      setResultData({ results: [] }); // Clear previous search results
-    }
-  }, [searchTerm, history]);
+  }, [headerTimeline]);
 
   return (
     <>
       <CssBaseline />
-      <Header
-        setSearchTerm={setSearchTerm}
-        existingTerm={searchTerm}
+      <SearchClient
+        urlParams={urlParams}
+        history={history}
         returnRef={returnRef}
-      />
-      <ResultsContainer
-        resultData={resultData}
-        returnRef={returnRef}
-        fetchMore={fetchMore}
-        loading={loading}
       />
     </>
   );
